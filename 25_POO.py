fef3fcf87480189d7fc1412fff8705f1848ab2fd
Jose David Nuñez Procchio <josedavidnup@gmail.com>
2023-01-23 13:38:20 -0400

    POO with Inherit

--- 25_POO.py
+++ 25_POO.py
@@ -1,23 +1,59 @@
 class Car:
-    length = 20
-    width = 120
-    tires = 4
-    on = False
+    def __init__(self):
+        self.__length = 20
+        self.__width = 120
+        self.__tires = 4
+        self.__on = False
 
-    def go(self):
-        self.on = True
+    def go(self, going):
+
+        self.__on = going
+
+        if self.__on:
+            checking = self.__checking_inside()
+
+        if self.__on and checking:
+            return "The car is going"
+        elif self.__on and checking == False:
+            return "Something went wrong"
+        else:
+            return "The car has stopped"
 
     def stop(self):
-        self.on = False
+        self.__on = False
 
     def status(self):
-        if self.on:
-            return "The car is going"
+        print(
+            f"The car has {self.__tires} tires. {self.__length} of length and {self.__width} of width."
+        )
+
+    def __checking_inside(self):
+        print("Checking engine")
+
+        self.gas = "ok"
+        self.oil = "ok"
+        self.door = "closed"
+
+        if self.gas == "ok" and self.oil == "ok" and self.door == "closed":
+            return True
+        else:
+            return False
 
 
 my_car = Car()
 
-print(my_car.length)
-print(my_car.tires)
-my_car.go()
+# print(my_car.length)
+# print(my_car.__tires)
+print(my_car.go(True))
 print(my_car.status())
+
+print(
+    "---------------------------Here it is the second object------------------------------------"
+)
+
+
+my_car2 = Car()
+# print(my_car2.length)
+# print(my_car2.__tires)
+print(my_car2.go(False))
+print(my_car2.status())
---
+++ 27_POO_Inherit_Super.py
@@ -0,0 +1,6 @@
+class Persona():
+    def __init__(self, nombre, edad, lugar_de_residencia):
+
+
+class Empleado():
+    pass
